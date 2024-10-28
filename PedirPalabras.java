c03b6bc7c1be5e68c2ec89e6925bef7d9bae36fe
FredyPinedaRodriguez <fgp8000@gmail.com>
2024-10-28 13:37:22 -0500

    Ejercicios clase 02

---
+++ PedirPalabras.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PedirPalabras {
+    public static void main(String[] args) {
+        var entrada = new Scanner(System.in);
+
+
+        
+        System.out.println("Introduzca palabra 1: ");
+        var palabra1 = entrada.nextLine();
+        System.out.println("Introduzca palabra 2: ");
+        var palabra2 = entrada.nextLine();
+        System.out.println("Introduzca palabra 3: ");
+        var palabra3 = entrada.nextLine();
+
+        System.out.println("palabra 1 que usted escribio: " +palabra1);
+        System.out.println("palabra 2 que usted escribio: " +palabra2);
+        System.out.println("palabra 3 que usted escribio: " +palabra3);
+System.out.println("FIN DEL EJERCICIO");
+    }
+}
